f8857d5b82ef0eeb614309a110a3437cec0d1fdc
matheuslima99 <maths.soares99@gmail.com>
2022-11-27 06:21:52 -0300

    Visual Structure - Component: SearchForm

---
+++ src/pages/Transactions/components/SearchForm/styles.ts
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+export const SearchFormContainer = styled.form`
+  display: flex;
+  gap: 1rem;
+
+  input {
+    flex: 1;
+    border-radius: 6px;
+    padding: 1rem;
+    outline: 0;
+    border: none;
+    color: ${(props) => props.theme["gray-100"]};
+    background: ${(props) => props.theme["gray-900"]};
+
+    &::placeholder {
+      color: ${(props) => props.theme["gray-500"]};
+    }
+  }
+
+  button {
+    padding: 0.875rem 2rem;
+    border-radius: 6px;
+    line-height: 0;
+    font-weight: 700;
+    cursor: pointer;
+    border: 1px solid ${(props) => props.theme["green-300"]};
+    color: ${(props) => props.theme["green-300"]};
+    background: transparent;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 0.75rem;
+
+    &:hover {
+      border-color: ${(props) => props.theme["green-500"]};
+      color: ${(props) => props.theme["white"]};
+      background: ${(props) => props.theme["green-500"]};
+      transition: background-color 0.2s, color 0.2s, border-color 0.2s;
+    }
+  }
+`;
