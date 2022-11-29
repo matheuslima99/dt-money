3f85c8812891c687e796088c382bdfcb4de6391e
matheuslima99 <maths.soares99@gmail.com>
2022-11-29 16:07:06 -0300

    API connection - Calculating transaction summary

--- src/components/Summary/SummaryCard.tsx
+++ src/components/Summary/SummaryCard.tsx
@@ -4,17 +4,18 @@ import { SummaryCardContainer, SummaryCardHeader } from "./styles";
 interface SumaryCardProps {
   variant?: "green";
   title: string;
+  value: number;
   icon: ReactNode;
 }
 
-export function SummaryCard({ title, icon, variant }: SumaryCardProps) {
+export function SummaryCard({ title, icon, variant, value }: SumaryCardProps) {
   return (
     <SummaryCardContainer variant={variant}>
       <SummaryCardHeader>
         <span>{title}</span>
         {icon}
       </SummaryCardHeader>
-      <strong>R$ 17.400,00</strong>
+      <strong>{value}</strong>
     </SummaryCardContainer>
   );
 }
--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -3,23 +3,47 @@ import { SummaryContainer } from "./styles";
 
 import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from "phosphor-react";
 import { useTheme } from "styled-components";
+import { useTransactions } from "../../hooks/useTransactions";
 
 export function Summary() {
   const theme = useTheme();
+  const { transactions } = useTransactions();
+
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === "income") {
+        acc.income += transaction.price;
+        acc.total += transaction.price;
+      } else {
+        acc.outcome += transaction.price;
+        acc.total -= transaction.price;
+      }
+
+      return acc;
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    }
+  );
 
   return (
     <SummaryContainer>
       <SummaryCard
+        value={summary.income}
         title="Entrada"
         icon={<ArrowCircleUp size={32} color={theme["green-300"]} />}
       />
 
       <SummaryCard
+        value={summary.outcome}
         title="Saídas"
         icon={<ArrowCircleDown size={32} color={theme["red-300"]} />}
       />
 
       <SummaryCard
+        value={summary.total}
         variant="green"
         title="Total"
         icon={<CurrencyDollar size={32} color={theme["white"]} />}
